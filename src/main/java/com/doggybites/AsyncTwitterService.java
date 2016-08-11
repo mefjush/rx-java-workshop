79768b4422ba623dc63f01c8ba1f3978458404f3
Mateusz Michalowski <mateusz@adhese.eu>
2016-08-11 21:27:17 +0200

    TwitterService

--- src/main/java/com/doggybites/AsyncTwitterService.java
+++ src/main/java/com/doggybites/AsyncTwitterService.java
@@ -1,8 +1,11 @@
 package com.doggybites;
 
 import rx.Observable;
+import rx.subjects.PublishSubject;
 
 public class AsyncTwitterService {
+
+    private final PublishSubject<Tweet> publishSubject = PublishSubject.create();
 
     private final TwitterService twitterService;
 
@@ -11,6 +14,16 @@ public class AsyncTwitterService {
     }
 
     Observable<Tweet> getTweets() {
-        return Observable.error(new RuntimeException("Not implemented!"));
+        publishSubject.doOnSubscribe(this::subscribe);
+        publishSubject.doOnUnsubscribe(this::unsubscribe);
+        return publishSubject.asObservable();
+    }
+
+    private synchronized void subscribe() {
+        //TODO
+    }
+
+    private synchronized void unsubscribe() {
+        //TODO
     }
 }
