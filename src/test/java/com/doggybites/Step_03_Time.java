21c046ad2ed8ebe83e18ebc7ee0bcd37e252384a
Mateusz Michalowski <mateusz@adhese.eu>
2016-08-16 08:29:25 +0200

    Solve 03

--- src/test/java/com/doggybites/Step_03_Time.java
+++ src/test/java/com/doggybites/Step_03_Time.java
@@ -34,6 +34,8 @@ public class Step_03_Time {
     @Test
     public void ticks_3_times_every_even_second() throws InterruptedException, UnsupportedEncodingException {
         //TODO
+        seconds.filter(i -> i % 2 == 0).take(3).toBlocking().forEach(out::println);
+        //or with latch and onComplete
 
         assertThat(outputStream.toString("UTF-8"), is("0\n2\n4\n"));
     }
@@ -41,6 +43,9 @@ public class Step_03_Time {
     @Test
     public void waits_2_seconds_and_then_generates_1_2_3() throws UnsupportedEncodingException {
         //TODO
+        Observable.just(1, 2, 3).delay(2, TimeUnit.SECONDS).toBlocking().forEach(out::println);
+        //or
+        //seconds.take(2).skip(2).concatWith(Observable.just(1L, 2L, 3L)).toBlocking().forEach(out::println);
 
         assertThat(outputStream.toString("UTF-8"), is("1\n2\n3\n"));
     }
